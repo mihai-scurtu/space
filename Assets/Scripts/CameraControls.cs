81fb4c6e0d51767689bbe6c4f6105d5663bc87b6
Mihai Scurtu <mscurtu90@gmail.com>
2015-11-30 15:56:44 +0200

    Add camera controls

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+    public float zoomIncrement = .5f;
+    public float dragSpeed = 2;
+
+    private Vector3 dragOrigin;
+
+    void Update() {
+        Pan();
+        Zoom();
+    }
+
+    void Pan() {
+        if (Input.GetMouseButtonDown(0)) {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+
+        transform.Translate(move, Space.World);
+    }
+
+    void Zoom() {
+        float zoom = -Input.mouseScrollDelta.y * zoomIncrement;
+
+        Camera.main.orthographicSize += zoom;
+    }
+}
